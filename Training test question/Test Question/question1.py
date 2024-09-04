1a06ba610dbb416f518dcbeec567770d5cb620c5
aloksingh1818 <138347516+aloksingh1818@users.noreply.github.com>
2024-09-04 14:47:22 +0530

    Delete Training test question/Test Question directory

--- Training test question/Test Question/question1.py
+++
@@ -1,9 +0,0 @@
-def count_substring(main_string, sub_string):
-    count = main_string.count(sub_string)
-    return count
-
-# Example usage
-main_string = "A B C D C D C"
-sub_string = "C D C"
-result = count_substring(main_string, sub_string)
-print(f"Sub_string '{sub_string}' occurs {result} times in the main string.")
